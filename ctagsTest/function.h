ae1108973e66d01ff5ce56bf50a04f8ecb914a2e
bw-bmbarga <bmbarga@brandwatch.com>
2018-06-18 10:45:10 +0200

    edit git ignore

--- ctagsTest/function.h
+++
@@ -1,6 +0,0 @@
-#ifndef FUNCTION_H
-# define FUNCTION_H
-
-void	say_hello(char *str);
-
-#endif
--- ctagsTest/main.c
+++
@@ -1,9 +0,0 @@
-#include "function.h"
-
-int		main(int ac, char **av)
-{
-	(void)ac;
-	(void)av;
-
-	say_hello("");
-}
